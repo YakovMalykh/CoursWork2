57d58d5ee43dbf55885ce9ec403be1fe9cb87ffb
yakov <to-yakoff@mail.ru>
2022-05-12 12:17:29 +0300

    создал проект

---
+++ src/main/java/com/example/courswork2/CoursWork2Application.java
@@ -0,0 +1,13 @@
+package com.example.courswork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursWork2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursWork2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courswork2/CoursWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courswork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
